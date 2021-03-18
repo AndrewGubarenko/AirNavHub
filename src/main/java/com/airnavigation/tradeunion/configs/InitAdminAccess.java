22af8d5bf19334c2595f301503bc1e9aed29e410
AndrewGubarenko <andrewgubarenko@gmail.com>
2021-03-18 16:01:17 +0200

    new day 2

--- src/main/java/com/airnavigation/tradeunion/configs/InitAdminAccess.java
+++ src/main/java/com/airnavigation/tradeunion/configs/InitAdminAccess.java
@@ -17,6 +17,7 @@ import javax.mail.MessagingException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Optional;
 
 @Component
 class InitAdminAccess {
@@ -41,38 +42,43 @@ class InitAdminAccess {
     @PostConstruct
     @Transactional
     protected void createDefaultUsers() {
-        String password = passwordGenerator.generateTemporaryPassword(30);
-        User user = adminRepository.save(
-                User.builder().username("andrewgubarenko@gmail.com")
-                        .password(passwordEncoder.encode(password))
-                        .firstName("Андрій Сергійович")
-                        .lastName("Губаренко")
-                        .count(0.00)
-                        .gender(Gender.MALE)
-                        .roles(new HashSet<>(Arrays.asList(Role.ADMINISTRATOR, Role.USER)))
-                        .build()
-        );
-        try {
-            emailService.sendMimeMessage(user.getUsername(),
-                    "Реєстрація користувача",
-                    "andrewgubarenko@gmail.com",
-                    new StringBuilder()
-                            .append("<html><body>")
+        Optional<User> userOpt = adminRepository.findByUsername("andrewgubarenko@gmail.com");
+        if(userOpt.isPresent()) {
+            return;
+        } else {
+            String password = passwordGenerator.generateTemporaryPassword(30);
+            User user = adminRepository.save(
+                    User.builder().username("andrewgubarenko@gmail.com")
+                            .password(passwordEncoder.encode(password))
+                            .firstName("Андрій Сергійович")
+                            .lastName("Губаренко")
+                            .count(0.00)
+                            .gender(Gender.MALE)
+                            .roles(new HashSet<>(Arrays.asList(Role.ADMINISTRATOR, Role.USER)))
+                            .build()
+            );
+            try {
+                emailService.sendMimeMessage(user.getUsername(),
+                        "Реєстрація користувача",
+                        "andrewgubarenko@gmail.com",
+                        new StringBuilder()
+                                .append("<html><body>")
                                 .append("<img src='cid:logo' alt='Logo' width='128' height='128'/>")
                                 .append("<H2>Вітаю! Вас зареєстровано на сайті профспілки Аеронавігація.</H2>")
                                 .append("<img src='cid:arrow' alt='Logo' width='200px'/>")
                                 .append("<p>Ваш тимчасовий пароль для доступу до особистого кабінету: ")
-                                    .append(password)
+                                .append(password)
                                 .append("</p>")
                                 .append("<p>Радимо змінити цей пароль на свій власний. </p>\n")
                                 .append("<p>Також радимо використовувати надійні паролі, наприклад ті, що генеруються Google.</p>")
                                 .append("<p>Увага! Цей лист згенеровано автоматично. Не відповідайте на нього.</p>")
                                 .append("<img src='cid:arrow' alt='Logo' width='200px'/>")
-                            .append("</body></html>").toString(),
-                    new ArrayList<>());
-        } catch (MessagingException ex) {
-            LOGGER.info("Something wrong with email. Unable to send an email to " + user.getUsername() + "\n" + ex.getLocalizedMessage());
+                                .append("</body></html>").toString(),
+                        new ArrayList<>());
+            } catch (MessagingException ex) {
+                LOGGER.info("Something wrong with email. Unable to send an email to " + user.getUsername() + "\n" + ex.getLocalizedMessage());
+            }
+            LOGGER.info("METHOD CREATE: User with username: " + user.getUsername() + " and access level:" + Role.ADMINISTRATOR.name() + " was created");
         }
-        LOGGER.info("METHOD CREATE: User with username: " + user.getUsername() + " and access level:" + Role.ADMINISTRATOR.name() + " was created");
     }
 }
