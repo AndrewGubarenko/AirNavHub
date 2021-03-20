43ca4b4e4e09ad2b723f8836724e231186ce3f9c
AndrewGubarenko <andrewgubarenko@gmail.com>
2021-03-20 18:35:36 +0200

    Ready to deploy

--- src/main/java/com/airnavigation/tradeunion/AirnavigationTradeunionWebResourceApplication.java
+++ src/main/java/com/airnavigation/tradeunion/AirnavigationTradeunionWebResourceApplication.java
@@ -2,9 +2,10 @@ package com.airnavigation.tradeunion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AirnavigationTradeunionWebResourceApplication {
+public class AirnavigationTradeunionWebResourceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AirnavigationTradeunionWebResourceApplication.class, args);
