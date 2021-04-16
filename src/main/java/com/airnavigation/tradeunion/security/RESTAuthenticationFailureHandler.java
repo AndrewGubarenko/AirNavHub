07260e373f8b367d049acfd5282cdb06c7b5a3c2
AndrewGubarenko <andrewgubarenko@gmail.com>
2021-04-16 19:43:29 +0300

    exception failure handler

--- src/main/java/com/airnavigation/tradeunion/security/RESTAuthenticationFailureHandler.java
+++ src/main/java/com/airnavigation/tradeunion/security/RESTAuthenticationFailureHandler.java
@@ -14,5 +14,6 @@ public class RESTAuthenticationFailureHandler extends SimpleUrlAuthenticationFai
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
         super.onAuthenticationFailure(request, response, exception);
+        response.sendRedirect("/user/logout");
     }
 }
